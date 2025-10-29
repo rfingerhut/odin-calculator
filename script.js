0d6bcea85342b3b0f6df2f99cc49d67403a6ee2f
Rachel Fingerhut <fingerhutrachel@gmail.com>
2025-10-28 19:15:07 -0700

    feat(operator buttons): Created object to hold problem information. Use operator buttons to do continuous caluclations.

--- script.js
+++ script.js
@@ -89,3 +89,45 @@ equalButton.addEventListener('click', () => {
     splitInput();
     input = '';
 });
+
+// when an operator button is pressed, do a calculation and then store the answer.
+// if it's an object ... problem would have properties: firstNum, operator, secondNum, and.. answer?
+// for each operator button is pressed, then you would have to check if a firstNum or secondNum exist?
+// problem.firstNum = 12 problem.operator = + problem.secondNum = 7 
+// if the minus button is pressed...
+// if there's a second number already, do the calculation. First number is only stored once the operator button is pressed.
+// operator button press: checks for first number, if no first number, assign it (need to store input as a concactonated string.) If there is not a firstNum, through an error. If there is a firstNum, check if secondNum exists. If secondNum does not exist, then continue with input collecting (no calculation). If a secondNum does exist, do the calculation using problem.firstNum, problem.operator, and problem.secondNum. Then, assign problem.operator with the current button that was pressed. Assign problem.FirstNum as the result from the calculation().
+
+const problem = {
+    firstNum:null,
+    secondNum:null,
+    operator:null,
+}
+
+
+const operatorButton = document.querySelectorAll('.operatorButton');
+operatorButton.forEach( el => el.addEventListener('click', () => {
+    let answer = '';
+    if (!problem.firstNum || !problem.operator || !problem.secondNum){
+        if (!problem.firstNum){
+            problem.firstNum = Number(input);
+            input='';
+            console.log('first number assigned: ' + problem.firstNum);
+        } else if (!problem.secondNum){
+            problem.secondNum = Number(input);
+            input = '';
+            console.log('second number assigned: ' + problem.secondNum);
+        }
+        if (!problem.operator){
+            problem.operator = el.textContent;
+            console.log('operator assigned: ' + problem.operator);
+        }  
+    }  
+    if (problem.firstNum && problem.operator && problem.secondNum){
+        answer = operate(problem.firstNum, problem.secondNum, problem.operator);
+        console.log(`Answer = ${answer}`);
+        problem.firstNum = answer;
+        problem.operator = null;
+        problem.secondNum = null;  
+    }
+}));
